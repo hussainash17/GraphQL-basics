83a7a7972c62d462686bc3f87a102f31ab72dab7
ashrafhussain17 <ashrafhussain.2317.csedu@gmail.com>
2020-09-15 22:01:24 +0600

    added some components

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import ApplloClient, { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+import App from './components/App'
+import SongList from './components/SongList';
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path='/' component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector('#root'));
