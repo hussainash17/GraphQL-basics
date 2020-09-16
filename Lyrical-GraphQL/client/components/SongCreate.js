31aa73950336eb41406e101d3278f5f401a47cc5
ashrafhussain17 <ashrafhussain.2317.csedu@gmail.com>
2020-09-16 10:39:30 +0600

    Song create component added

--- Lyrical-GraphQL/client/components/SongCreate.js
+++ Lyrical-GraphQL/client/components/SongCreate.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import { Link, hashHistory } from 'react-router';
-import query from '../queries/fetchSongs';
+// import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = { title: '' };
   }
--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -4,8 +4,9 @@ import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApplloClient, { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
-import App from './components/App'
+import App from './components/App';
 import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
 
 const client = new ApolloClient({});
 
@@ -15,6 +16,7 @@ const Root = () => {
       <Router history={hashHistory}>
         <Route path='/' component={App}>
           <IndexRoute component={SongList} />
+          <Route path='song/new' component={SongCreate} />
         </Route>
       </Router>
     </ApolloProvider>
